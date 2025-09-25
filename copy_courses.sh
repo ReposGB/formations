f7a0c909fafdbd6e678bb0bc031bcc4185945738
Ghislain BIDAUT <ghislain.bidaut@inserm.fr>
2025-09-25 19:31:58 +0200

    update

--- copy_courses.sh
+++ copy_courses.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Cours
 cp -p  /Users/bidaut/CRCMBox/Cours/Cours\ Master\ Oncologie\ M1\ Tech\ Santé\ Module\ Génomique/Cours-Master-M1-Genomique-Variants_RNA_Seq.html ./cours
 cp -p  /Users/bidaut/CRCMBox/Cours/M2\ Bio\ Sante\ -\ Parcours\ IA\ Biomarqeurs/M2-Bio-Sante-IA-Biomarkers-Classif-Genomics-Data.html  ./cours
 cp -p /Users/bidaut/CRCMBox/Cours/Cours\ M2\ Technologie\ de\ la\ Santé\ -\ Module\ Génomique\ -\ Bertucci\ -\ RNA-Seq/Cours-M2_Technologies-de-la-Santé_RNA-Seq_Single-Cell.html ./cours/
 cp -p /Users/bidaut/CRCMBox/Cours/M2\ Biologie\ Sante\ TC\ Génomique/Cours-Intro-Bioinfo-Onco-M2-Biologie-Santé-Tronc-Commun-Genomique.html ./cours/
 cp -p /Users/bidaut/CRCMBox/Cours/Cours\ Réseaux\ et\ Maladies\ -\ Master\ Technologie\ Santé/M2-Biologie-Santé-Genomique-Réseaux-Biologiques-et-Maladies-Humaines.pdf ./cours/
+
+# TD
+
+cp -rp  /Users/bidaut/CRCMBox/Cours/TD\ Master\ Bioinfo\ DNA-Seq/TD\ DNA-seq\ Master\ Bioinfo\ 2024/TD_Master_Bioinfo_DNA-seq_appel_de_variants.html ./cours/
