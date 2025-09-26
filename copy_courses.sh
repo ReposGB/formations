18f05607ae7491d4a77fe791ccd38a146fb03c08
Ghislain BIDAUT <ghislain.bidaut@inserm.fr>
2025-09-26 14:43:56 +0200

    update

--- copy_courses.sh
+++ copy_courses.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Cours
-cp -p  /Users/bidaut/CRCMBox/Cours/Cours\ Master\ Oncologie\ M1\ Tech\ Santé\ Module\ Génomique/Cours-Master-M1-Genomique-Variants_RNA_Seq.html ./cours
-cp -p  /Users/bidaut/CRCMBox/Cours/M2\ Bio\ Sante\ -\ Parcours\ IA\ Biomarqeurs/M2-Bio-Sante-IA-Biomarkers-Classif-Genomics-Data.html  ./cours
-cp -p /Users/bidaut/CRCMBox/Cours/Cours\ M2\ Technologie\ de\ la\ Santé\ -\ Module\ Génomique\ -\ Bertucci\ -\ RNA-Seq/Cours-M2_Technologies-de-la-Santé_RNA-Seq_Single-Cell.html ./cours/
-cp -p /Users/bidaut/CRCMBox/Cours/M2\ Biologie\ Sante\ TC\ Génomique/Cours-Intro-Bioinfo-Onco-M2-Biologie-Santé-Tronc-Commun-Genomique.html ./cours/
-cp -p /Users/bidaut/CRCMBox/Cours/Cours\ Réseaux\ et\ Maladies\ -\ Master\ Technologie\ Santé/M2-Biologie-Santé-Genomique-Réseaux-Biologiques-et-Maladies-Humaines.pdf ./cours/
+cp -p   /Users/bidaut/CRCMBox/Cours/Cours\ Master\ Oncologie\ M1\ Tech\ Santé\ Module\ Génomique/Cours-Master-M1-Genomique-Variants_RNA_Seq.html  ./cours/Cours-M1-Variants_DNA_Seq_RNA_Seq.html
+cp -p  /Users/bidaut/CRCMBox/Cours/M2\ Bio\ Sante\ -\ Parcours\ IA\ Biomarqeurs/M2-Bio-Sante-IA-Biomarkers-Classif-Genomics-Data.html ./cours/Course-M2-Classif-Genomics-Data.html
+cp -p /Users/bidaut/CRCMBox/Cours/Cours\ M2\ Technologie\ de\ la\ Santé\ -\ Module\ Génomique\ -\ Bertucci\ -\ RNA-Seq/Cours-M2_Technologies-de-la-Santé_RNA-Seq_Single-Cell.html ./cours/Cours-M2_RNA-Seq_Single-Cell.html
+cp -p /Users/bidaut/CRCMBox/Cours/M2\ Biologie\ Sante\ TC\ Génomique/Cours-Intro-Bioinfo-Onco-M2-Biologie-Santé-Tronc-Commun-Genomique.html ./cours/Cours-M2-Intro-Bioinfo-Onco.html
+cp -p /Users/bidaut/CRCMBox/Cours/Cours\ Réseaux\ et\ Maladies\ -\ Master\ Technologie\ Santé/M2-Biologie-Santé-Genomique-Réseaux-Biologiques-et-Maladies-Humaines.pdf ./cours/Course-M2-Networks-Human-Disease.pdf
 
 # TD
 
